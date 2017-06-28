822f51d41c077f3bc93dc8045f8ad46fbcf0e167
shyphile <123207680@qq.com>
2017-06-28 09:07:35 +0800

    06280

--- js/index.js
+++ js/index.js
@@ -38,19 +38,31 @@
         $('a').attr('href', "");
     }
 
+    if ($('ol:first').css('list-style-type') != 'none') { /* For IE6/7 only. */
+        $('ol ol').each(function(i, ol) {
+            ol = $(ol);
+            var level1 = ol.closest('li').index() + 1;
+            ol.children('li').each(function(i, li) {
+                li = $(li);
+                var level2 = level1 + '.' + (li.index() + 1);
+                li.prepend('<span>' + level2+ '</span>');
+            });
+        });
+    }
+
     //$('li>a').click(function(){
     //    var iframepage=document.getElementById('iframepage');
     //    setIframeHeight(iframepage);
     //});
 });
 
- function iFrameHeight() {
-    var ifm = document.getElementById("iframepage");
-    var subWeb = document.frames ? document.frames["iframepage"].document : ifm.contentDocument;
-    if (ifm != null && subWeb != null) {
-        ifm.height = subWeb.body.scrollHeight;
-    }
-}
+//  function iFrameHeight() {
+//     var ifm = document.getElementById("iframepage");
+//     var subWeb = document.frames ? document.frames["iframepage"].document : ifm.contentDocument;
+//     if (ifm != null && subWeb != null) {
+//         ifm.height = subWeb.body.scrollHeight;
+//     }
+// }
 
 function displayCount() {
     if ($('#count a').attr('href') == 'http://count.knowsky.com') {
@@ -59,41 +71,41 @@ function displayCount() {
     $('#count').show();
 }
 
-var timer1;
-var timeCount = 0;
-function reinitIframe() 
-{
-    timeCount++;
-    if (timeCount > 100) 
-    {
-        window.clearInterval(timer1);// 停止定时
-    }
-    var iframe = document.getElementById("iframepage");
-    var bHeight = iframe.contentWindow.document.body.scrollHeight;
-    var dHeight = iframe.contentWindow.document.documentElement.scrollHeight;
-    var height = Math.min(bHeight, dHeight);
-    iframe.height = height;
-}
+// var timer1;
+// var timeCount = 0;
+// function reinitIframe() 
+// {
+//     timeCount++;
+//     if (timeCount > 100) 
+//     {
+//         window.clearInterval(timer1);// 停止定时
+//     }
+//     var iframe = document.getElementById("iframepage");
+//     var bHeight = iframe.contentWindow.document.body.scrollHeight;
+//     var dHeight = iframe.contentWindow.document.documentElement.scrollHeight;
+//     var height = Math.min(bHeight, dHeight);
+//     iframe.height = height;
+// }
 
-function reinitIframeEND() {
-    timeCount = 0;
-    window.clearInterval(timer1);// 停止定时
-    var iframe = document.getElementById("iframepage");
-    var bHeight = iframe.contentWindow.document.body.scrollHeight;
-    var dHeight = iframe.contentWindow.document.documentElement.scrollHeight;
-    var height = Math.min(bHeight, dHeight);
-    iframe.height = height;
-    timer1 = window.setInterval("reinitIframe()", 500); //定时开始
-}
+// function reinitIframeEND() {
+//     timeCount = 0;
+//     window.clearInterval(timer1);// 停止定时
+//     var iframe = document.getElementById("iframepage");
+//     var bHeight = iframe.contentWindow.document.body.scrollHeight;
+//     var dHeight = iframe.contentWindow.document.documentElement.scrollHeight;
+//     var height = Math.min(bHeight, dHeight);
+//     iframe.height = height;
+//     timer1 = window.setInterval("reinitIframe()", 500); //定时开始
+// }
 
- // document.domain = "caibaojian.com";
- function setIframeHeight(iframe) {
-     if (iframe) {
-         var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
-         if (iframeWin.document.body) {
-             iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight;
-             alert(iframe.height );
-         }
-     }
- }
+//  // document.domain = "caibaojian.com";
+//  function setIframeHeight(iframe) {
+//    if (iframe) {
+//        var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
+//        if (iframeWin.document.body) {
+//            iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight;
+//            alert(iframe.height );
+//        }
+//    }
+// }
 
